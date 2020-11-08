7d60cfd7b254da3774aa96bf81bd2a70e92dc2fb
Artur Molenda <ar2rweb@gmail.com>
2020-11-08 18:35:33 +0100

    connect mongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+export default connectDb;
