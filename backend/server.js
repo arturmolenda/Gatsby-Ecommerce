aa6b90820c05c26aba8615fcf1c8cd30d37cd6d7
Artur Molenda <ar2rweb@gmail.com>
2020-11-09 14:33:04 +0100

    add error handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import morgan from 'morgan';
 import products from './data/products.js';
+import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 dotenv.config();
 
 connectDB();
@@ -18,9 +20,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use('/api/users', userRoutes);
+
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
