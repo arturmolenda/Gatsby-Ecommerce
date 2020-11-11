9397084e9457aa9d43bcf575f384838b27c1efee
Artur Molenda <ar2rweb@gmail.com>
2020-11-11 17:36:42 +0100

    register screen with functionality and redux

---
+++ frontend/src/pages/register.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Register from "../components/screens/Register"
+const register = () => {
+  return <Register />
+}
+
+export default register
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -7,9 +7,10 @@ import {
 } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { userLoginReducer } from "./reducers/userReducers"
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers"
 const reducer = combineReducers({
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 let userInfoFromStorage
