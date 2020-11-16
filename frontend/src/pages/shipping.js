272df9dec29ad0ed7d5c2059b721d25cef5123de
Artur Molenda <ar2rweb@gmail.com>
2020-11-16 14:15:53 +0100

    shipping screen

---
+++ frontend/src/pages/shipping.js
@@ -0,0 +1,13 @@
+import { Container } from "@material-ui/core"
+import React from "react"
+import Shipping from "../components/screens/Shipping"
+
+const shipping = () => {
+  return (
+    <Container>
+      <Shipping />
+    </Container>
+  )
+}
+
+export default shipping
