0e18cb8d3e119a106e8f400bc8c8ac8cd447550a
Artur Molenda <ar2rweb@gmail.com>
2020-11-21 13:33:41 +0100

    Users admin screen, fully functional

---
+++ frontend/src/components/DeleteDialog.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core"
+
+const DeleteDialog = ({
+  open,
+  handleClose,
+  title,
+  description,
+  handleDelete,
+  actionBtn = "Delete",
+}) => {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <div style={{ minWidth: 300 }}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          {description && <DialogContentText>{description}</DialogContentText>}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="secondary" variant="contained">
+            {actionBtn}
+          </Button>
+        </DialogActions>
+      </div>
+    </Dialog>
+  )
+}
+
+export default DeleteDialog
--- frontend/src/components/screens/adminScreens/Users.js
+++ frontend/src/components/screens/adminScreens/Users.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react"
 
-import { Link, navigate } from "gatsby"
+import { navigate } from "gatsby"
 
 import { useDispatch, useSelector } from "react-redux"
-import { addToCart, removeFromCart } from "../../../redux/actions/cartActions"
+import {
+  deleteUser,
+  listAllUsers,
+  updateUserPermissions,
+} from "../../../redux/actions/userActions"
+import {
+  USER_DELETE_RESET,
+  USER_UPDATE_ADMIN_RESET,
+} from "../../../redux/constants/userConstants"
 
 import {
   Button,
-  Card,
   Checkbox,
-  Collapse,
   Grid,
   makeStyles,
   Paper,
@@ -19,15 +25,15 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TextField,
   Tooltip,
   Typography,
 } from "@material-ui/core"
 import DeleteIcon from "@material-ui/icons/Delete"
 import SaveIcon from "@material-ui/icons/Save"
 import { Alert } from "@material-ui/lab"
-import { listAllUsers } from "../../../redux/actions/userActions"
+
 import Loader from "../../Loader"
+import DeleteDialog from "../../DeleteDialog"
 
 const useStyles = makeStyles(() => ({
   tableHeadText: {
@@ -52,120 +58,201 @@ const useStyles = makeStyles(() => ({
       marginRight: 16,
     },
   },
+  alert: {
+    marginBottom: 8,
+  },
 }))
 
 const Users = () => {
   const [updatedUsers, setUpdatedUsers] = useState([])
+  const [userToDelete, setUserToDelete] = useState(null)
   const classes = useStyles()
 
   const dispatch = useDispatch()
   const { userInfo } = useSelector(state => state.userLogin)
   const { loading, users, error } = useSelector(state => state.userListAll)
+  const {
+    loading: updateLoading,
+    success: updateSuccess,
+    error: updateError,
+  } = useSelector(state => state.userUpdateAdmin)
+  const {
+    loading: deleteLoading,
+    success: deleteSuccess,
+    error: deleteError,
+  } = useSelector(state => state.userDelete)
+
+  const resetAlerts = () => {
+    dispatch({ type: USER_UPDATE_ADMIN_RESET })
+    dispatch({ type: USER_DELETE_RESET })
+  }
 
   useEffect(() => {
     if (!userInfo || !userInfo.isAdmin) navigate("/login")
-    else if (!users || users.length === 0) dispatch(listAllUsers())
-  }, [userInfo])
+    else if (!users || users.length === 0 || updateSuccess || deleteSuccess)
+      dispatch(listAllUsers())
+  }, [userInfo, updateSuccess, deleteSuccess])
 
-  // useEffect(() => {
-  //   if (users && users.length !== 0) {
-  //     setUsersData(() => {
-  //       const obj = users.map(x => )
-  //     })
-  //   }
-  // }, [users])
+  useEffect(() => {
+    return () => {
+      resetAlerts()
+    }
+  }, [])
 
   const changeHandle = (e, i) => {
-    // if(e.target.checked !== users[i].isAdmin){
     setUpdatedUsers(prevState => {
       prevState[users[i]._id] = e.target.checked
       return { ...prevState }
     })
-    // }
   }
-  console.log(updatedUsers)
 
+  const updatePermissionsHandle = (userId, isAdmin) => {
+    resetAlerts()
+    dispatch(updateUserPermissions(userId, isAdmin))
+  }
+
+  const deleteHandle = () => {
+    resetAlerts()
+    dispatch(deleteUser(userToDelete))
+    setUserToDelete(null)
+  }
+
+  console.log(updatedUsers)
   return (
-    userInfo &&
-    userInfo.isAdmin && (
-      <Grid container justify="center">
+    <Grid container justify="center">
+      {userInfo && userInfo.isAdmin && (
         <Grid item md={10} sm={12} xs={12}>
           <Typography variant="h1">USERS</Typography>
+
           {loading ? (
             <Loader />
-          ) : error ? (
-            <Alert severity="error">{error}</Alert>
           ) : (
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow className={classes.tableHeadText}>
-                    <TableCell>ID</TableCell>
-                    <TableCell>NAME</TableCell>
-                    <TableCell>EMAIL</TableCell>
-                    <TableCell>ADMIN</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {users &&
-                    users.length !== 0 &&
-                    users.map((user, i) => {
-                      console.log(user, user.isAdmin)
-                      return (
-                        <TableRow className={classes.tableBackground}>
-                          <TableCell>{user._id}</TableCell>
-                          <TableCell>{user.name}</TableCell>
-                          <TableCell>{user.email}</TableCell>
-                          <TableCell>
-                            {console.log(user.isAdmin)}
-                            <Checkbox
-                              color="secondary"
-                              defaultChecked={user.isAdmin}
-                              onChange={e => changeHandle(e, i)}
-                            />
-                          </TableCell>
-                          <TableCell align="justify">
-                            <Tooltip title="Save" placement="top">
-                              <span className={classes.iconBtn}>
-                                <Button
-                                  color="primary"
-                                  variant="contained"
-                                  size="small"
-                                  disabled={
-                                    !(
-                                      typeof updatedUsers[user._id] !==
-                                        "undefined" &&
-                                      updatedUsers[user._id] !== user.isAdmin
-                                    )
-                                  }
-                                >
-                                  <SaveIcon />
-                                </Button>
-                              </span>
-                            </Tooltip>
-                            <Tooltip title="Delete" placement="top">
-                              <span className={classes.iconBtn}>
-                                <Button
-                                  color="secondary"
-                                  variant="contained"
-                                  size="small"
-                                >
-                                  <DeleteIcon />
-                                </Button>
-                              </span>
-                            </Tooltip>
-                          </TableCell>
-                        </TableRow>
-                      )
-                    })}
-                </TableBody>
-              </Table>
-            </TableContainer>
+            <>
+              {error && (
+                <Alert className={classes.alert} severity="error">
+                  {error}
+                </Alert>
+              )}
+              {updateError && (
+                <Alert className={classes.alert} severity="error">
+                  {updateError}
+                </Alert>
+              )}
+              {updateSuccess && (
+                <Alert className={classes.alert} severity="success">
+                  User updated!
+                </Alert>
+              )}
+              {deleteError && (
+                <Alert className={classes.alert} severity="error">
+                  {deleteError}
+                </Alert>
+              )}
+              {deleteSuccess && (
+                <Alert className={classes.alert} severity="success">
+                  User deleted!
+                </Alert>
+              )}
+              <TableContainer component={Paper} style={{ marginTop: 10 }}>
+                <Table>
+                  <TableHead>
+                    <TableRow className={classes.tableHeadText}>
+                      <TableCell>ID</TableCell>
+                      <TableCell>NAME</TableCell>
+                      <TableCell>EMAIL</TableCell>
+                      <TableCell>ADMIN</TableCell>
+                      <TableCell></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {users &&
+                      users.length !== 0 &&
+                      users.map((user, i) => {
+                        console.log(user, user.isAdmin)
+                        return (
+                          <TableRow className={classes.tableBackground}>
+                            <TableCell>{user._id}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>
+                              {console.log(user.isAdmin)}
+                              <Checkbox
+                                color="secondary"
+                                defaultChecked={
+                                  typeof updatedUsers[user._id] !== "undefined"
+                                    ? updatedUsers[user._id]
+                                    : user.isAdmin
+                                }
+                                onChange={e => changeHandle(e, i)}
+                                disabled={user._id === userInfo._id}
+                              />
+                            </TableCell>
+                            <TableCell align="justify">
+                              <Tooltip title="Save" placement="top">
+                                <span className={classes.iconBtn}>
+                                  <Button
+                                    color="primary"
+                                    variant="contained"
+                                    size="small"
+                                    disabled={
+                                      !(
+                                        typeof updatedUsers[user._id] !==
+                                          "undefined" &&
+                                        updatedUsers[user._id] !== user.isAdmin
+                                      ) || updateLoading === user._id
+                                    }
+                                    onClick={() =>
+                                      updatePermissionsHandle(
+                                        user._id,
+                                        updatedUsers[user._id]
+                                      )
+                                    }
+                                  >
+                                    <SaveIcon />
+                                    {updateLoading === user._id && (
+                                      <Loader button />
+                                    )}
+                                  </Button>
+                                </span>
+                              </Tooltip>
+                              <Tooltip title="Delete" placement="top">
+                                <span className={classes.iconBtn}>
+                                  <Button
+                                    color="secondary"
+                                    variant="contained"
+                                    size="small"
+                                    disabled={
+                                      user._id === userInfo._id ||
+                                      deleteLoading === user._id
+                                    }
+                                    onClick={() => setUserToDelete(user._id)}
+                                  >
+                                    <DeleteIcon />
+                                    {deleteLoading === user._id && (
+                                      <Loader button />
+                                    )}
+                                  </Button>
+                                </span>
+                              </Tooltip>
+                            </TableCell>
+                          </TableRow>
+                        )
+                      })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
           )}
         </Grid>
-      </Grid>
-    )
+      )}
+      <DeleteDialog
+        title={"Are you sure?"}
+        description={"This action cannot be undone"}
+        open={Boolean(userToDelete)}
+        handleClose={() => setUserToDelete(null)}
+        handleDelete={deleteHandle}
+      />
+    </Grid>
   )
 }
 
