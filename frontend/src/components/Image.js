94c01a32aa952f6880791aea0dd20b0930bff1aa
Artur Molenda <ar2rweb@gmail.com>
2020-11-27 21:53:22 +0100

    bug fix after new img file

--- frontend/src/components/Image.js
+++ frontend/src/components/Image.js
@@ -1,44 +1,87 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { Img as ReactImage } from "react-image"
+import { Skeleton } from "@material-ui/lab"
 
-// Note: You can change "images" to whatever you'd like.
-
-const Image = props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        images: allFile {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                fluid(maxWidth: 1200) {
-                  ...GatsbyImageSharpFluid
+const Image = props => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          images: allFile {
+            edges {
+              node {
+                relativePath
+                name
+                childImageSharp {
+                  fluid(maxWidth: 1200) {
+                    ...GatsbyImageSharpFluid
+                  }
                 }
               }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(props.filename)
-      })
-      if (!image) {
-        return null
-      }
-      return (
-        <Img
-          alt={props.alt}
-          fluid={image.node.childImageSharp.fluid}
-          style={props.customStyle}
-        />
-      )
-    }}
-  />
-)
+      `}
+      render={data => {
+        console.log(data.images)
+        const image = data.images.edges.find(n => {
+          return n.node.relativePath.includes(props.filename)
+        })
+
+        if (!image) {
+          try {
+            const src = require(`../../public/images/${props.filename}`)
+            if (src) {
+              const placeholder = data.images.edges.find(n => {
+                return n.node.relativePath.includes("productPlaceholder.jpg")
+              })
+
+              return (
+                <ReactImage
+                  style={{ width: "100%" }}
+                  alt={props.alt}
+                  src={src}
+                  loader={
+                    <div style={{ position: "relative" }}>
+                      <div style={{ visibility: "hidden" }}>
+                        <Img
+                          alt={props.alt}
+                          fluid={placeholder.node.childImageSharp.fluid}
+                          style={props.customStyle}
+                        />
+                      </div>
+                      <Skeleton
+                        variant="rect"
+                        style={{
+                          position: "absolute",
+                          top: 0,
+                          height: "100%",
+                          width: "100%",
+                          zIndex: 111,
+                        }}
+                      />
+                    </div>
+                  }
+                />
+              )
+            }
+          } catch (error) {
+            return null
+          }
+        }
+        console.log(image)
+        return (
+          <Img
+            alt={props.alt}
+            fluid={image.node.childImageSharp.fluid}
+            style={props.customStyle}
+          />
+        )
+      }}
+    />
+  )
+}
 
 export default Image
