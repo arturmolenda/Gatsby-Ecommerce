4853e698f64f5b16b26f4db6ced81ebe3acf6eca
Artur Molenda <ar2rweb@gmail.com>
2020-11-27 21:51:56 +0100

    product create and product image uplaod redux

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
@@ -8,6 +11,9 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_IMAGE_UPLOAD_FAIL,
+  PRODUCT_IMAGE_UPLOAD_REQUEST,
+  PRODUCT_IMAGE_UPLOAD_SUCCESS,
 } from "../constants/productConstants"
 import axios from "axios"
 
@@ -77,6 +83,63 @@ export const deleteProduct = id => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const createProduct = product => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_CREATE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post(`/api/products`, product, config)
+    dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const uploadProductImage = formData => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_IMAGE_UPLOAD_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    formData.map(async img => await axios.post(`/api/upload`, img, config))
+
+    dispatch({ type: PRODUCT_IMAGE_UPLOAD_SUCCESS })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_IMAGE_UPLOAD_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
