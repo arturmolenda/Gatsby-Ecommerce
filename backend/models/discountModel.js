d8cae4160f5cb1b8ac1bb972761ca4de59633765
Artur Molenda <ar2rweb@gmail.com>
2020-12-01 11:47:52 +0100

    discount backend api

--- backend/models/discountModel.js
+++ backend/models/discountModel.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 
 const discountSchema = mongoose.Schema(
   {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     code: { type: String, required: true, unique: true },
     isPercent: { type: Boolean, required: true, default: true },
     minPrice: { type: Number, required: true, default: 0 },
     amount: { type: Number, required: true },
     expireDate: { type: Date, required: true },
-    couponsAmount: { type: Number, required: false, default: 999999 },
-    onePerUser: { type: Boolean, required: false, default: false },
+    couponsAmount: { type: Number, required: true, default: 999999 },
+    onePerUser: { type: Boolean, required: true, default: false },
     isActive: { type: Boolean, required: true, default: true },
   },
   {
