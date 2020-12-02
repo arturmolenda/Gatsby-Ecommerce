bd528ba2b02b97dfe349cc4c2c8d7e2d6598a920
Artur Molenda <ar2rweb@gmail.com>
2020-12-02 21:49:57 +0100

    pagination and keyword redux

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -23,11 +23,13 @@ import {
 } from "../constants/productConstants"
 import axios from "axios"
 
-export const listProducts = () => async dispatch => {
+export const listProducts = (page = "", keyword = "") => async dispatch => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
-    const { data } = await axios.get("/api/products")
+    const { data } = await axios.get(
+      `/api/products?pageNumber=${page}&keyword=${keyword}`
+    )
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
   } catch (error) {
