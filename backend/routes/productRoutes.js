78102967726e2947e6dc9f97bac998c28dc3fa38
Artur Molenda <ar2rweb@gmail.com>
2020-12-07 23:16:14 +0100

    getTopProducts backend route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,14 @@ import {
   getAllProducts,
   getProducts,
   editProduct,
+  getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.get('/all', protect, admin, getAllProducts);
+router.get('/top', getTopProducts);
 router
   .route('/:id')
   .delete(protect, admin, deleteProduct)
