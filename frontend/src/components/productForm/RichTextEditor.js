cf1507a0cb8a5beba59ed98d1cadf0f51fe34faa
Artur Molenda <ar2rweb@gmail.com>
2020-12-13 12:33:24 +0100

    add CKEditor 5

---
+++ frontend/src/components/productForm/RichTextEditor.js
@@ -0,0 +1,100 @@
+import React from "react"
+import CKEditor from "@ckeditor/ckeditor5-react"
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { InputLabel, makeStyles } from "@material-ui/core"
+import { useState } from "react"
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: 15,
+    position: "relative",
+    [theme.breakpoints.down("xs")]: {
+      overflow: "auto",
+    },
+    "& .ck-editor__editable_inline": {
+      minHeight: 100,
+      backgroundColor: "rgb(0 0 0 / 10%) !important",
+      transition: "background-color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms",
+      "&:hover": {
+        backgroundColor: "rgb(0 0 0 / 15%) !important",
+      },
+    },
+    "& .ck-editor__editable_inline > *": {
+      marginTop: "20px !important",
+    },
+    "& .ck-editor__editable_inline > p": {
+      fontSize: 16,
+    },
+    "& .ck-focused": {
+      borderColor: "#9d9d9d !important",
+    },
+    "& .ck-toolbar_grouping": {
+      borderTopLeftRadius: "4px !important",
+      borderTopRightRadius: "4px !important",
+    },
+  },
+  inputLabel: {
+    zIndex: -1,
+    position: "absolute",
+    top: 38,
+    left: -3,
+    transform: "translate(12px, 17px) scale(0.91)",
+    color: "#646464",
+  },
+  inputLabelFocused: {
+    transform: "translate(12px, 7px) scale(0.75)",
+  },
+}))
+
+const RichTextEditor = ({ description, setDescription }) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const classes = useStyles()
+
+  const focusInputField = () => {
+    document.getElementsByClassName("ck-editor__main")[0].click()
+  }
+
+  return (
+    <div className={classes.container}>
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          toolbar: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "blockQuote",
+            "link",
+            "numberedList",
+            "bulletedList",
+            "alignment",
+            "insertTable",
+            "|",
+            "undo",
+            "redo",
+          ],
+        }}
+        data={description}
+        onChange={(event, editor) => {
+          const data = editor.getData()
+          setDescription(data)
+        }}
+        onBlur={() => setIsFocused(false)}
+        onFocus={() => setIsFocused(true)}
+      />
+      <InputLabel
+        focused={isFocused}
+        shrink={isFocused}
+        className={`${classes.inputLabel} ${
+          (isFocused || description !== "") && classes.inputLabelFocused
+        }`}
+        onClick={focusInputField}
+      >
+        Description
+      </InputLabel>
+    </div>
+  )
+}
+
+export default RichTextEditor
