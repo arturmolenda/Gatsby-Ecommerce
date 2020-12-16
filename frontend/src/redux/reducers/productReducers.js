4d5b67f60980f23545da26746b9e98fff1ef41b2
Artur Molenda <ar2rweb@gmail.com>
2020-12-16 15:32:27 +0100

    products reset redux

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -33,6 +33,10 @@ import {
   PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_REVIEW_FAIL,
   PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_RESET_REQUEST,
+  PRODUCT_RESET_SUCCESS,
+  PRODUCT_RESET_FAIL,
+  PRODUCT_RESET_RESET,
 } from "../constants/productConstants"
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -180,6 +184,21 @@ export const productReviewCreateReducer = (state = {}, action) => {
     case PRODUCT_CREATE_REVIEW_FAIL:
       return { loading: false, error: action.payload }
     case PRODUCT_CREATE_REVIEW_RESET:
+      return {}
+    default:
+      return state
+  }
+}
+
+export const productReset = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_RESET_REQUEST:
+      return { loading: true }
+    case PRODUCT_RESET_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_RESET_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_RESET_RESET:
       return {}
     default:
       return state
