71ce53d9ca37b96bdb8a0bdaef1a163a943fca0f
Artur Molenda <ar2rweb@gmail.com>
2020-12-19 13:40:34 +0100

    deployment preparation

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,6 @@ const app = express();
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-  app.use(morgan('dev'));
-}
-
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,6 +29,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/public')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend/public/index.html'))
+  );
+} else if (process.env.NODE_ENV === 'development') {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+  app.use(morgan('dev'));
+}
 
 app.use(notFound);
 
