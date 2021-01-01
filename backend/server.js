b96dc2126c82b38882a956f1026de08d2d6d2cf6
Artur Molenda <ar2rweb@gmail.com>
2021-01-01 14:44:50 +0100

    fix heroku error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-import morgan from 'morgan';
 import orderRoutes from './routes/orderRoutes.js';
 import discountRoutes from './routes/discountRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
@@ -35,11 +34,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend/public/index.html'))
   );
-} else if (process.env.NODE_ENV === 'development') {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-  app.use(morgan('dev'));
 }
 
 app.use(notFound);
